e2f06dd92428d004169d789c24041566552db9b9
User <user@Users-MacBook.local>
2023-08-03 13:02:03 +0200

    try

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -15,7 +15,7 @@ class FileStorage:
                 cls = eval(cls)
             obj_dict = {}
             for key, value in self.__objects.items():
-                if cls == value.__class__.__name__:
+                if type(value) == cls:
                     obj_dict[key] = value
             return obj_dict
         return self.__objects
