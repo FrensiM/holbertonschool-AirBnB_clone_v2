bc8344a73353e2ae73b38a1d9fb7edd0658f18f2
User <user@Users-MacBook.local>
2023-08-14 13:10:15 +0200

    C-is fun

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+''' flask setup'''
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    return 'HBNB'
+
+@app.route('/c/<text>')
+def str_txt(text):
+    formatted_text = text.replace('_', ' ')
+    result = f"C {formatted_text}"
+    return result
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
