9f78b51682ade7e793fe03d68d7a333efe0ed2ba
User <user@Users-MacBook.local>
2023-08-17 14:11:36 +0200

    add

---
+++ web_flask/9-states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+''' flask setup'''
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+from models.city import City
+from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
+
+app = Flask(__name__)
+
+
+@app.route('/states', strict_slashes=False)
+def state_ls():
+    states = storage.all("State").values()
+    return render_template('9-states.html', states=states)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def state_id(id):
+    state = None
+    states = storage.all("State").values()
+    for obj in states:
+        if obj.id == id:
+            state = obj
+    return render_template('9-states.html', state=state)
+
+
+@app.teardown_appcontext
+def storage_close(self):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
